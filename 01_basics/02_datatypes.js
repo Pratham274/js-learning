0464cb510377b2386116388b556f52c281e37625
Pratham Gupta <69028389+Pratham274@users.noreply.github.com>
2026-04-15 04:46:42 +0000

    added 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version 
+// alert(3+3)  // we are using node.js not browser.
+console.log(3+3) 
+console.log("Pratham")
+
+let name="Pratham"
+let age=21
+let isLoggedIn=false
+let state=null
+
+
+//PRIMITIVE
+//number=> 2^53
+//bigInt
+//string=>"" ,''
+//boolean => true/false
+//null=>standalone value
+//undefined=> variable is declared but value is not defined
+//symbol=> unique
+
+//object
+//console.log(typeof "Pratham")
+//console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
