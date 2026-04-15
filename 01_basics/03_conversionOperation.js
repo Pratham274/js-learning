acf30702178899cd4e031f6be07a7251780b36cf
Pratham Gupta <69028389+Pratham274@users.noreply.github.com>
2026-04-15 05:16:55 +0000

    conversions added

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="Pratham"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true=> 1, false=> 0
+
+let isLoggedIn=""
+let boolIsloggedIn=Boolean(isLoggedIn)
+// console.log(boolIsloggedIn)
+
+// 1=> true
+// 0=> false
+//""=> false
+// "Hello"=> true
+
+let num=33
+let stringnum=String(num)
+console.log(typeof stringnum)
