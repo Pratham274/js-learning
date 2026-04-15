f49864c92583babb4f3eec99c464eb36c6e37fff
Pratham Gupta <69028389+Pratham274@users.noreply.github.com>
2026-04-15 04:52:43 +0000

    01_variables.js added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=1434232
+let accountEmail="pg06042005@gmail.com"
+var accountPassword="12345"
+accountCity="Kanpur"
+
+//accountId=2 //not allowed
+accountEmail="hchc@gmail.com"
+accountPassword="2134232"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
