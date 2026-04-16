5b04a003a396bcfb80cc5b15a77b36583ca7ed11
Pratham Gupta <69028389+Pratham274@users.noreply.github.com>
2026-04-16 13:31:50 +0000

    summary added

---
+++ 01_basics/summary.js
@@ -0,0 +1,37 @@
+//Datatypes
+// 1.Primitive 
+//7 types: Strings(call by value),Number,Boolean,Null,Undefined,Symbol,BigInt
+//2.Reference type(Non-Primitive)
+// Arrays, Objects, Functions
+
+//We never define language or datatype in JS,dynamically typed
+
+// const score=100
+// const scoreValue=10.3 // also a number
+// const isLogged=false
+// const outsidetemp=null
+// let userEmail; //undefined
+
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+
+// console.log(id===anotherId);
+
+//const bigNumber=465456468465n
+
+const heros=["Shaktiman","Krrish","Flying Jatt","Naagraj"];
+let myObj={
+    name:"Pratham",
+    age:21,
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+
+// console.log(typeof bigNumber);
+// console.log(typeof outsidetemp);
+// console.log(typeof scoreValue);
+// console.log(typeof myFunction);
+
+// console.log(typeof anotherId);
