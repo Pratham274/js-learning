fb56cabd3c4a7a2a91d7f8e86b7b9cfcc3bde06d
Pratham Gupta <69028389+Pratham274@users.noreply.github.com>
2026-04-16 11:04:07 +0000

    comparisions.js added

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+//console.log(null==0); //when it comes to equality check , it does not assigned 0 to null.
+// console.log(null>=0);
+//comparisions convert null to a number, treating it as 0,
+//That's why null>=0 is true and null>0 is false
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//Strict check, also check datatypes as well as values
+// console.log("2"==2);
+// console.log("2"===2);
