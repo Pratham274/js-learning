9898676c233cc74098f8ca8a5557653260dc1a79
Pratham Gupta <69028389+Pratham274@users.noreply.github.com>
2026-04-16 10:48:44 +0000

    conversions and operations added

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,4 +21,43 @@ let boolIsloggedIn=Boolean(isLoggedIn)
 
 let num=33
 let stringnum=String(num)
-console.log(typeof stringnum)
+// console.log(typeof stringnum)
+
+//*******************Operations *************************/
+
+let value=3
+let negvalue=-value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// coneole.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1="Hello"
+// let str2="Pratham"
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log("2"+1);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+4*5%3);
+
+// console.log(true);
+// console.log(+true);
+//console.log(true+) // error
+//console.log(+"");
+
+// let num1, num2, num3
+
+// num1=num2=num3=2+2;
+
+let gameCounter=100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
