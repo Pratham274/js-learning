5be9d35aeb4468c4df6b336a7b5a14dc954400c0
Pratham Gupta <69028389+Pratham274@users.noreply.github.com>
2026-06-02 07:38:04 +0000

    new files added

---
+++ 05_Iterations/six.js
@@ -0,0 +1,32 @@
+// const coding=["js","ruby","java","python","cpp"]
+
+//forEach does not return the values.
+// const values=coding.forEach( (item) => {
+//     // console.log(item);
+//     return item;
+// })
+
+// console.log(values);
+
+
+//filter(takes callback()) (True or False)
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+//it will return 
+// let newNums=myNums.filter( (num) => num>4)
+// console.log(newNums);
+
+//it will not return as scope is started. Write return
+// let newNums=myNums.filter( (num) => {
+//     return num>4
+// })
+// console.log(newNums);
+
+// const newNums=[]
+// myNums.forEach( (num) => {
+//     if(num>4){
+//         newNums.push(num)
+//     }
+
+// })
+// console.log(newNums);
+
